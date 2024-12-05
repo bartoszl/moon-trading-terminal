b9541ddd6114d78de293b8d2f170ffffbe59cc30
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2024-12-05 22:28:27 +0100

    feat: restructure tests a bit

--- test/confirm.test.ts
+++ test/confirm.test.ts
@@ -4,38 +4,11 @@ import { app } from "../src/app";
 import {FixedSide} from "@wen-moon-ser/moonshot-sdk-evm";
 import { erc20 } from "../src/abi/erc20";
 
-describe("App tests", () => {
+describe("[/Confirm]", () => {
   const tokenAddress = '0xD22248Cc09b468F69a65d4c519099699049dA242'
   const moonshotContract = '0xA103455889D4e22600c208D6125E0E7673106695'
 
-  it('[/prepare] Transaction object is correct', async () => {
-    const provider = new JsonRpcProvider(process.env.RPC_URL);
-    const signer = new Wallet(process.env.PRIVATE_KEY as string, provider);
-
-    const tokenAmount = '100';
-    const slippage = 50000; // 5%
-
-    const response = await request(app).post("/prepare").send({
-      walletAddress: signer.address,
-      tokenAmount: parseUnits(tokenAmount).toString(),
-      slippageBps: slippage,
-      tradeDirection: 'BUY',
-      tokenAddress,
-      fixedSide: FixedSide.OUT,
-    });
-
-    expect(response.status).toEqual(200);
-    expect(response.body.value).toBeTruthy();
-    expect(response.body.to).toEqual('0xA103455889D4e22600c208D6125E0E7673106695');
-    expect(response.body.data).toBeTruthy();
-    expect(response.body.nonce).toBeTruthy();
-    expect(response.body.gasPrice).toBeTruthy();
-    expect(response.body.from).toEqual(signer.address);
-    expect(response.body.chainId).toEqual(8453);
-    expect(response.body.gasLimit).toBeTruthy();
-  })
-
-  it('Prepare and send BUY + Fixed Out', async () => {
+  it('Buy FixedOut and Sell FixedIn', async () => {
     const provider = new JsonRpcProvider(process.env.RPC_URL);
     const signer = new Wallet(process.env.PRIVATE_KEY as string, provider);
     const erc20tokenContract = new ethers.Contract(tokenAddress, erc20, signer);
@@ -108,7 +81,7 @@ describe("App tests", () => {
     expect(tokenBalanceAfterBuy - tokenBalanceAfterSell).toEqual(tokenDifference)
   });
 
-  it('Prepare and send BUY + Fixed In', async () => {
+  it('Buy FixedIn', async () => {
     const provider = new JsonRpcProvider(process.env.RPC_URL);
     const signer = new Wallet(process.env.PRIVATE_KEY as string, provider);
 
@@ -132,4 +105,6 @@ describe("App tests", () => {
     expect(confirmResponse.status).toEqual(200);
     expect(confirmResponse.body.status).toEqual(1);
   });
+
+  // TODO: Test Sell FixedOut
 })
---
+++ test/prepare.test.ts
@@ -0,0 +1,37 @@
+import { Wallet, JsonRpcProvider, parseUnits } from "ethers";
+import request from "supertest";
+import { app } from "../src/app";
+import {FixedSide} from "@wen-moon-ser/moonshot-sdk-evm";
+
+describe("[/Prepare]", () => {
+  const tokenAddress = '0xD22248Cc09b468F69a65d4c519099699049dA242'
+
+  it('[BUY, OUT] Transaction object is correct', async () => {
+    const provider = new JsonRpcProvider(process.env.RPC_URL);
+    const signer = new Wallet(process.env.PRIVATE_KEY as string, provider);
+
+    const tokenAmount = '100';
+    const slippage = 50000; // 5%
+
+    const response = await request(app).post("/prepare").send({
+      walletAddress: signer.address,
+      tokenAmount: parseUnits(tokenAmount).toString(),
+      slippageBps: slippage,
+      tradeDirection: 'BUY',
+      tokenAddress,
+      fixedSide: FixedSide.OUT,
+    });
+
+    expect(response.status).toEqual(200);
+    expect(response.body.value).toBeTruthy();
+    expect(response.body.to).toEqual('0xA103455889D4e22600c208D6125E0E7673106695');
+    expect(response.body.data).toBeTruthy();
+    expect(response.body.nonce).toBeTruthy();
+    expect(response.body.gasPrice).toBeTruthy();
+    expect(response.body.from).toEqual(signer.address);
+    expect(response.body.chainId).toEqual(8453);
+    expect(response.body.gasLimit).toBeTruthy();
+  })
+
+  // TODO: Check the rest of the transactions In/Out Buy/Sell
+})
